2c44140f213d2150d9ccc5474d4ea0d7f5eec219
Basheer Thiabat <bcheer@yahoo.com>
2023-02-01 03:49:30 +0300

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -6,14 +6,11 @@ const Home = ()=>{
     return(
         <div className = 'home-page-div'>
         <div className = 'welcome-text'>
-        <p>Suspendisse ultrices mauris ac augue tempor porta. Donec nec commodo diam. Pellentesque vitae felis eu elit mattis pretium 
-        id ut nunc. Maecenas tempus iaculis purus, at pellentesque libero luctus at. Phasellus pellentesque eleifend ultricies. 
-        Aenean viverra tellus vitae mi elementum, at tincidunt neque condimentum. Donec et urna non elit commodo dictum et vitae dui.
-        Sed mi tortor, consectetur a vulputate vel, pellentesque quis nisi. Vivamus aliquam auctor dui, vel volutpat orci ultrices
-        ac. Aenean in turpis commodo tellus commodo dapibus quis et diam. Donec eget convallis orci, non vehicula sem. Morbi at
-        mauris non ipsum maximus rhoncus eu vel ante. Pellentesque justo dui, vulputate a diam sit amet, tempor interdum magna.
-        Cras consequat,   nibh eget accumsan congue, orci mauris iaculis ex, nec lacinia libero nunc ut magna. Praesent sed 
-        maximus felis.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
         </div>
         <div className = 'welcome-image'>
         <img src = {image}/>
